3f2e3671f627eb5bb8baffc480b5aa703c8b9eac
张志文 <1727080961@qq.com>
2021-11-21 14:11:02 +0800

    Week03 作业: 1.（必做）整合你上次作业的 httpclient/okhttp。

---
+++ 03Netty/src/main/java/http/work01/client/OkHttpClientMain.java
@@ -0,0 +1,41 @@
+package http.work01.client;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @ClassName OkHttpClientMain
+ * @Description TODO
+ * @Date 2021/11/13 21:38
+ * @Author kevin
+ **/
+public class OkHttpClientMain {
+
+    public static void main(String[] args) {
+        OkHttpClient client = new OkHttpClient.Builder()
+                .connectTimeout(30, TimeUnit.SECONDS)
+                .callTimeout(120, TimeUnit.SECONDS)
+                .pingInterval(5, TimeUnit.SECONDS)
+                .readTimeout(60, TimeUnit.SECONDS)
+                .writeTimeout(60, TimeUnit.SECONDS)
+                .build();
+
+        String url = "http://localhost:8808/nio";
+        Request request = new Request.Builder()
+                .url(url)
+                .build();
+
+        try (Response response = client.newCall(request).execute()) {
+            System.out.println("请求"+url+"的响应结果：");
+            System.out.println(response.body().string());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+}
---
+++ 03Netty/src/main/java/http/work01/netty/NettyHttpInitializer.java
@@ -0,0 +1,24 @@
+package http.work01.netty;
+
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPipeline;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.handler.codec.http.HttpObjectAggregator;
+import io.netty.handler.codec.http.HttpServerCodec;
+
+/**
+ * @ClassName NettyHttpInitializer
+ * @Description TODO
+ * @Date 2021/11/13 23:01
+ * @Author kevin
+ **/
+public class NettyHttpInitializer extends ChannelInitializer<SocketChannel> {
+
+    @Override
+    protected void initChannel(SocketChannel ch) {
+        ChannelPipeline p = ch.pipeline();
+        p.addLast(new HttpServerCodec());
+        p.addLast(new HttpObjectAggregator(1024 * 1024));
+        p.addLast(new NettyHttpHandler());
+    }
+}
